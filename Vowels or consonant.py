131070b4add61af41b26bdf681fc36f3885416e7
MOHAMMEDSIMADH <47002875+MOHAMMEDSIMADH@users.noreply.github.com>
2019-01-25 08:39:10 -0800

    Update and rename Vowels or consonant to Vowels or consonant.py

---
+++ Vowels or consonant.py
@@ -0,0 +1,11 @@
+#MSD
+#vowels or consonant
+x=input()
+x.lower()
+if x.isalpha():
+   if a=='a' or a=='e' or a=='i' or a=='o' or a=='u':
+      print("Vowel")
+   else :
+       print("consonant")
+else:
+    print ("Invalid")
