c8117da05e1e61e982d441f3f08fa54646a9df51
MOHAMMEDSIMADH <47002875+MOHAMMEDSIMADH@users.noreply.github.com>
2025-05-17 14:09:20 +0530

    Delete Even or Odd.py

--- Even or Odd.py
+++
@@ -1,9 +0,0 @@
-#MSD
-#Even or Odd
-b=int(input())
-if b%2==0 and b>0:
-    print("The given num is even")
-elif b%2==1 and b>0:
-    print("The given num is odd")
-else:
-    print("The given number is Invalid")
